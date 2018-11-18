4c55ee7fad926433f4f3ff9768d8167edb0395a3
Michael Blanton <mblanton@gmail.com>
2018-11-17 21:53:23 -0800

    More fixes

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ class BugsnagLogger extends Transport {
 
       if (this.silent) return callback(null, true);
       if (!(info.level in this._levelsMap)) return callback(null, true);
+      info.meta.severity = this._levelsMap[info.level];
       this.bugsnag.notify(new Error(info.message), info.meta, function() {
         callback(null, true);
       });
